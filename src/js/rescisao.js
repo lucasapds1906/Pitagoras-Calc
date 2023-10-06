427c1f6c37b38f020ca9a8c2f88d299bbc0ef99b
Lucas Silva <lucasapds1906@gmail.com>
2023-10-06 00:37:02 -0300

    projeto final

--- src/js/rescisao.js
+++ src/js/rescisao.js
@@ -22,34 +22,29 @@ const descontosTotaisTabela = document.getElementById("descontos_totais_tabela")
 // LINHA SALARIO LIQUIDO
 const salarioLiquidoTabela = document.getElementById("salario-liquido_tabela");
 
-let descontoTotal = 0;
-
-function feriasProporcionais(salarioBruto, mesesTrabalhados) {
+const feriasProporcionais = (salarioBruto, mesesTrabalhados) => {
     return  salarioBruto * mesesTrabalhados / 12;
 }
 
-function umTerçoFerias(feriasProporcionais) {
+const umTerçoFerias = (feriasProporcionais) => {
     return feriasProporcionais / 3;
 }
 
-function decimoTerceiroProporcional(salarioBruto, mesesTrabalhados) {
+const decimoTerceiroProporcional = (salarioBruto, mesesTrabalhados) => {
     return salarioBruto / 12 * mesesTrabalhados;
 }
 
-function verbasRescisoria(feriasProporcionais, umTerçoFerias, decimoTerceiroProporcional) {
+const verbasRescisoria = (feriasProporcionais, umTerçoFerias, decimoTerceiroProporcional) => {
     return feriasProporcionais + umTerçoFerias + decimoTerceiroProporcional;
 }
 
+const depositoFgts = (salarioBruto, mesesTrabalhados) => {
+    return salarioBruto * 0.08 * mesesTrabalhados;
+}
 
-
-
-
-
-
-
-
-
-
+const multaFgts = (depositoFgts) => {
+    return depositoFgts * 0.4;
+}
 
 // FUNÇÃO INSS
 function inss(salarioBruto) {
@@ -57,88 +52,50 @@ function inss(salarioBruto) {
 
     if (salarioBruto > 0) {
         if (salarioBruto <= 1302) {
-            aliquotaInssTabela.innerHTML = `7.5%`;
             inss = salarioBruto * 0.075;
         } else if (salarioBruto <= 2571.29) {
-            aliquotaInssTabela.innerHTML = `9%`;
             inss = salarioBruto * 0.09;
         } else if (salarioBruto <= 3856.94) {
-            aliquotaInssTabela.innerHTML = `12%`;
             inss = salarioBruto * 0.12;
         } else if (salarioBruto <= 7507.49) {
-            aliquotaInssTabela.innerHTML = `14%`;
             inss = salarioBruto * 0.14;
         } else {
-            aliquotaInssTabela.innerHTML = `TETO`;
             inss = 876.97;
         }
     }
 
-    descontoTotal += inss;
     return inss;
-}
-
-// FUNÇÃO IRRF
-function irrf(base, dependentes) {
-    //  PARA NÃO RETORNAR NAN QUANDO SUBTRAIR POR 0
-    const verificaIndependentes = (base, dependentes) => {
-        if (dependentes > 0) {
-            return base - (dependentes * 189.59);
-        } else {
-            return base;
-        }
-    }
-
-    const baseIrrf = verificaIndependentes(base, dependentes)
-    let irrf = 0;
-
-    if (baseIrrf < 1903.98) {
-        aliquotaIrffTabela.innerHTML = `ISENTO`;
-        irrf = 0;
-    } else if (baseIrrf <= 2826.65) {
-        aliquotaIrffTabela.innerHTML = `7.5%`;
-        irrf = baseIrrf * 0.075;
-    } else if (baseIrrf <= 3751.05) {
-        aliquotaIrffTabela.innerHTML = `15%`;
-        irrf = baseIrrf * 0.15;
-    } else if (baseIrrf <= 4664.68) {
-        aliquotaIrffTabela.innerHTML = `22.5%`;
-        irrf = baseIrrf * 0.225;
-    }
-    else {
-        aliquotaIrffTabela.innerHTML = `27.5%`;
-        irrf = baseIrrf * 0.275
-    }
-
-    descontoTotal += irrf;
-    return [baseIrrf - irrf, irrf];
 }
 
 // FUNCTION MAIN DO PROGRAMA
 function calcular() {
     tabela.classList.remove("d-none")
     divisor.classList.remove("d-none")
-    descontoTotal = 0;
+
     const salarioBruto = parseFloat(document.getElementById("salario-bruto_input").value);
     const meses = parseFloat(document.getElementById("meses_input").value);
 
 // VERBAS RESCISÓRIAS
     const feriasProp = feriasProporcionais(salarioBruto, meses);
     const tercoFerias = umTerçoFerias(feriasProp);
-    const decTerceiro = decimoTerceiroProporcional(salarioBruto, meses)
-    const verbas = verbasRescisoria(feriasProp, tercoFerias, decTerceiro)
+    const decTerceiro = decimoTerceiroProporcional(salarioBruto, meses);
+    const verbas = verbasRescisoria(feriasProp, tercoFerias, decTerceiro);
 
 //  DEDUÇÕES
-    const inssDecTerceiro = inss(decTerceiro)
-    console.log(inssDecTerceiro)
+    const inssDecTerceiro = inss(decTerceiro);
 
     // FGTS
+    const deposito = depositoFgts(salarioBruto, meses);
+    const multa = multaFgts(deposito);
+    const totalFgts = deposito + multa;
 
-
-
+    const rescisao = verbas + totalFgts - inssDecTerceiro;
 
     // TABELA
-    
+    document.getElementById("verbas_rescisorias_tabela").innerHTML = `${verbas.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}`;
+    document.getElementById("descontos_tabela").innerHTML = `${inssDecTerceiro.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}`;
+    document.getElementById("total_fgts_tabela").innerHTML = `${totalFgts.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}`;
+    document.getElementById("total_tabela").innerHTML = `${rescisao.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}`;
 }
 
 // LIMPAR CAMPOS e TABELA
@@ -147,17 +104,13 @@ function limpar() {
     divisor.classList.add("d-none")
     // CAMPOS
     document.getElementById("salario-bruto_input").value = "";
-    document.getElementById("dependetes_input").value = "";
+    document.getElementById("meses_input").value = "";
 
     // TABELA
-    descontosInssTabela.innerHTML = "-";
-    descontosIrrfTabela.innerHTML = "-";
-    salarioLiquidoTabela.innerHTML = "-";
-    proventosTotaisTabela.innerHTML = "-";
-    proventosSalarioBrutoTabela.innerHTML = "-";
-    descontosTotaisTabela.innerHTML = "-";
-    aliquotaInssTabela.innerHTML = "-";
-    aliquotaIrffTabela.innerHTML = "-";
+    document.getElementById("verbas_rescisorias_tabela").innerHTML = "-";
+    document.getElementById("descontos_tabela").innerHTML = "-";
+    document.getElementById("total_fgts_tabela").innerHTML = "-";
+    document.getElementById("total_tabela").innerHTML = "-";
 }
 
 // BOTAO CALCULAR
